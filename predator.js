4ac0b1806fbb4e3ab48862facc20acede075b367
jeongwonho64 <attemptn63@gmail.com>
2024-07-21 19:49:44 +0800

    finished predator boid
    done with the simulation

--- predator.js
+++ predator.js
@@ -80,5 +80,110 @@ class Predator {
       endShape(CLOSE);
       pop();
     }
-
-}
+    
+    // A method that calculates a steering force towards a target
+    // STEER = DESIRED MINUS VELOCITY
+    seek(target) {
+          // A vector pointing from the location to the target
+      
+      let desired = this.position.sub(target); 
+      let d = desired.mag();
+  
+      // Steering = Desired minus Velocity
+      let steer = p5.Vector.sub(desired, this.velocity);
+      steer.limit(this.maxforce);  // Limit to maximum steering force
+      this.applyForce(steer);
+      
+    }
+    
+    //checks if a predator is hitting the boid passed to the function 
+    eating(boidX, boidY, size) {
+      if(boidX == null) console.log("null");
+      let boidP1 = createVector(boidX,  boidY - size*2);
+      let boidP2 = createVector(boidX - size, boidY + size * 2);
+      let boidP3 = createVector(boidX + size, boidY + size * 2);
+      let boidPoints = [boidP1, boidP2, boidP3];
+      
+      let predatorP1 = createVector(this.position.x, this.position.y - this.size * 2);
+      let predatorP2 = createVector(this.position.x - this.size, this.position.y + this.size * 2);
+      let predatorP3 = createVector(this.position.x + this.size, this.position.y + this.size * 2);
+       let predatorPoints = [predatorP1, predatorP2, predatorP3];
+      
+      return this.polyPoly(boidPoints, predatorPoints);
+    }  
+    
+    polyPoly(p1, p2) {
+  
+    // go through each of the vertices, plus the next
+    // vertex in the list
+    let next = 0;
+    for (let current = 0; current < p1.length; current++) {
+  
+      // get next vertex in list
+      // if we've hit the end, wrap around to 0
+      next = current+1;
+      if (next == p1.length) next = 0;
+  
+      // get the PVectors at our current position
+      // this makes our if statement a little cleaner
+      let vc = p1[current];    // c for "current"
+      let vn = p1[next];       // n for "next"
+  
+      // now we can use these two points (a line) to compare
+      // to the other polygon's vertices using polyLine()
+      let collision = this.polyLine(p2, vc.x,vc.y,vn.x,vn.y);
+      if(collision) return true; //<------problem line
+    }
+  
+    return false;
+  }
+  
+  
+  // POLYGON/LINE
+   polyLine(vertices, x1, y1, x2, y2) {
+  
+    // go through each of the vertices, plus the next
+    // vertex in the list
+    let next = 0;
+    for (let current = 0; current < vertices.length; current++) {
+  
+      // get next vertex in list
+      // if we've hit the end, wrap around to 0
+      next = current+1;
+      if (next == vertices.length) next = 0;
+  
+      // get the PVectors at our current position
+      // extract X/Y coordinates from each
+      let x3 = vertices[current].x;
+      let y3 = vertices[current].y;
+      let x4 = vertices[next].x;
+      let y4 = vertices[next].y;
+  
+      // do a Line/Line comparison
+      // if true, return 'true' immediately and
+      // stop testing (faster)
+      let hit = this.lineLine(x1, y1, x2, y2, x3, y3, x4, y4);
+      if (hit) return true;
+    }
+  
+    // never got a hit
+    return false;
+  }
+  
+  
+  // LINE/LINE
+   lineLine(x1, y1, x2, y2, x3, y3, x4, y4) {
+  
+    // calculate the direction of the lines
+    let uA = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
+    let uB = ((x2-x1)*(y1-y3) - (y2-y1)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
+  
+    // if uA and uB are between 0-1, lines are colliding
+    if (uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1) return true;
+    return false;
+  }
+    
+  }
+  
+  
+  
